289616a6cffd2a7b33f79623973cf4c8e022df98
mac_ken <anken914_3@ezweb.ne.jp>
2025-10-27 16:32:56 +0900

    ルートの設定と各ページの最小構成を作成

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,27 +1,18 @@
-import { useState } from "react";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Work from './pages/Work';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div>
-        <button
-          className="bg-blue-500 text-white p-4"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/work" element={<Work />} />
+      </Routes>
+    </Router>
   );
 }
 
